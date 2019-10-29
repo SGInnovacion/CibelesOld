c587eddf77113f4b1ea8f3266cb1d137a03b3635
Alfonso Jiménez <aljimenez97@gmail.com>
2019-10-29 15:49:01 +0100

    :sparkles: Arrabal Felipe II

--- intentHandlers/protection.js
+++ intentHandlers/protection.js
@@ -1,6 +1,6 @@
 const { planeamientoNdp, bdcSearch } = require('../APIs');
 
-module.exports = (agent) => {
+const general = agent => {
 
     console.log('[INFO] is protected request handler: ?');
     console.log(agent.parameters);
@@ -10,22 +10,22 @@ module.exports = (agent) => {
         console.log(a);
         let NDP = a.codigoNdps;
         return planeamientoNdp(NDP).then(response => {
-        	console.log(response);
-        	const catalogo = response.catalogo;
-        	const patrimonioHistorico = response.patrimonioHistorico;
-        	const perteneceArrabalFelipeII = response.parcela.perteneceArrabalFelipeII === 'true';
-        	const perteneceAPE0001 = response.parcela.perteneceAPE0001 === 'true';
+            console.log(response);
+            const catalogo = response.catalogo;
+            const patrimonioHistorico = response.patrimonioHistorico;
+            const perteneceArrabalFelipeII = response.parcela.perteneceArrabalFelipeII === 'true';
+            const perteneceAPE0001 = response.parcela.perteneceAPE0001 === 'true';
             console.log('[INFO] Catalogo');
             console.log(catalogo);
             console.log('[INFO] Patrimonio historico');
-        	console.log(patrimonioHistorico);
+            console.log(patrimonioHistorico);
             console.log('[INFO] Pertenece felipe o ape:');
             console.log(perteneceArrabalFelipeII, perteneceAPE0001);
-        	let speechText = '';
-        	const anyCatalogue = Object.keys(catalogo).find( key => catalogo[key].proteccionActual !== undefined);
+            let speechText = '';
+            const anyCatalogue = Object.keys(catalogo).find( key => catalogo[key].proteccionActual !== undefined);
             const affectedPatrimonio = patrimonioHistorico && patrimonioHistorico.length !== 0;
 
-        	speechText += anyCatalogue ? `La protección del edificio como ${anyCatalogue} es ${catalogo[anyCatalogue].proteccionActual} ` : 'El edificio no está protegido ';
+            speechText += anyCatalogue ? `La protección del edificio como ${anyCatalogue} es ${catalogo[anyCatalogue].proteccionActual} ` : 'El edificio no está protegido ';
             speechText += affectedPatrimonio ? 'y está afectado por patrimonio de la Comunidad de Madrid. '
                 : 'y no está afectado por patrimonio de la Comunidad de Madrid. ';
 
@@ -36,9 +36,36 @@ module.exports = (agent) => {
             } else if(perteneceArrabalFelipeII) {
                 speechText += 'Además, pertenece al Arrabal de Felipe II.';
             }
-        	agent.add(speechText);
+            agent.add(speechText);
         }).catch( e => {
-        	console.log(e.message)
+            console.log(e.message)
         })
     })
+};
+
+const felipe = agent => {
+    console.log('[INFO] Protection.felipe request handler: ');
+    console.log(agent.parameters);
+    let street = agent.parameters.address || 'alcala 23';
+
+    return bdcSearch(street).then(a => {
+        console.log(a);
+        let NDP = a.codigoNdps;
+        return planeamientoNdp(NDP).then(response => {
+            console.log(response);
+            const perteneceArrabalFelipeII = response.parcela.perteneceArrabalFelipeII === 'true';
+            console.log('[INFO] Pertenece felipe o ape:');
+            console.log(perteneceArrabalFelipeII);
+            const speechText = perteneceArrabalFelipeII ? `Sí, ${street} pertenece al Arrabal de Felipe II.` : `No, ${street} no pertenece al Arrabal de Felipe II.`;
+            console.log(speechText);
+            agent.add(speechText);
+        }).catch( e => {
+            console.log(e.message)
+        })
+    })
+};
+
+module.exports = {
+    general,
+    felipe
 };
