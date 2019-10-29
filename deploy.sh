e345015518f6751a1452d80a7ded50e188def37f
Daniel Carlander-Reuterfelt Gallo <d.cgallo@alumnos.upm.es>
2019-10-29 17:11:51 +0100

    :pencil: Remove crap left from AWS-CLI installation

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 zip -ru dialogflow-webhook.zip *
 echo 'Compressed. Deploying to AWS Lambda...'
 aws lambda update-function-code --function-name auto-webhook --zip-file fileb://./dialogflow-webhook.zip > /dev/null
-echo 'Deployment Complete'
+echo 'Deployment Complete'	
