343c1cfb8aacc85989474cf9b49b5e7b8f0b35ca
Alfonso Jiménez <aljimenez97@gmail.com>
2019-11-03 10:57:45 +0100

    :see_no_evil: .gitignore updated

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 zip -ru webhook.zip *
 echo 'Compressed. Deploying Dialogflow to AWS Lambda...'
 aws configure set region us-east-2
+echo 'Region changed to us-east-2'
 aws lambda update-function-code --function-name auto-webhook --zip-file fileb://./webhook.zip > /dev/null
 echo 'Done!'
-echo 'Compressed. Deploying Alexa to AWS Lambda...'
+echo 'Deploying Alexa to AWS Lambda...'
 aws configure set region us-east-1
+echo 'Region changed to us-east-1'
 aws lambda update-function-code --function-name ayto-webhook --zip-file fileb://./webhook.zip > /dev/null
 echo 'Done!'
 echo 'Deployment Complete'
