aff2ed12e49b528af9e987038d534afeb8065a7a
Daniel Carlander-Reuterfelt Gallo <d.cgallo@alumnos.upm.es>
2019-11-04 18:17:11 +0100

    🐛 Arreglar que no accede al expediente vigente

--- intentHandlers/record.js
+++ intentHandlers/record.js
@@ -17,7 +17,7 @@ module.exports = (street) => {
             console.log('[INFO] Last record:');
             console.log(lastRecord);
 
-            let speechText = current ? `El expediente vigente en ${street} es el ${current.numero} con denominación ${current.denominacion}` :
+            let speechText = current ? `El expediente vigente en ${street} es el ${current[0].numero} con denominación ${current[0].denominacion}` :
                 lastRecord ? `El último expediente del histórico en ${street} es el ${lastRecord.numero} con denominación ${lastRecord.denominacion}` :
                     `No dispongo de información sobre expedientes para ${street}`;
 
