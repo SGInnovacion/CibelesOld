ed65ddedb65c04e15a38ffeeb1dbbc2514bba83a
Daniel Carlander-Reuterfelt Gallo <d.cgallo@alumnos.upm.es>
2019-11-04 18:05:44 +0100

    💚 Enhance to only deploy to dialogflow if wanted

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,25 @@
-#!/bin/bash
-
 zip -ru webhook.zip *
- 
+
 if [ "$#" -eq  "0" ]; then
-    echo 'Compressed. Deploying Dialogflow to AWS Lambda...'
-	aws configure set region us-east-2
-	echo 'Region changed to us-east-2'
-	aws lambda update-function-code --function-name auto-webhook --zip-file fileb://./webhook.zip > /dev/null
-	echo 'Done!'
+	
+	echo 'Compressed.'
 	echo 'Deploying Alexa to AWS Lambda...'
 	aws configure set region us-east-1
 	echo 'Region changed to us-east-1'
 	aws lambda update-function-code --function-name ayto-webhook --zip-file fileb://./webhook.zip > /dev/null
 	echo 'Done!'
-	echo 'Deployment Complete'
-else
-    echo 'Compressed. Deploying Dialogflow to AWS Lambda...'
+
+	echo 'Deploying Dialogflow to AWS Lambda...'
 	aws configure set region us-east-2
 	echo 'Region changed to us-east-2'
 	aws lambda update-function-code --function-name auto-webhook --zip-file fileb://./webhook.zip > /dev/null
 	echo 'Done!'
+	echo 'Deployment Complete'
+	
+else
+	echo 'Compressed. Deploying Dialogflow to AWS Lambda...'
+	aws configure set region us-east-2
+	echo 'Region changed to us-east-2'
+	aws lambda update-function-code --function-name auto-webhook --zip-file fileb://./webhook.zip > /dev/null
 	echo 'Deployment Complete'
 fi
