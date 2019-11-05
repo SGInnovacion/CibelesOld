d1b74620214600e96661275399784f07fa22149e
Alfonso Jiménez <aljimenez97@gmail.com>
2019-11-05 19:25:04 +0100

    ♻️ Async to intentHandlers protection

--- intentHandlers/protection.js
+++ intentHandlers/protection.js
@@ -1,158 +1,112 @@
-const { planeamientoNdp, bdcSearch } = require('../APIs');
+const { planeamientoNdp, bdcSearch, getPlaneamiento } = require('../APIs');
 
-const general = street => {
+const general = async street => {
+    let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
+    const response = address.planeamiento;
+    const catalogo = response.planeamiento.catalogo;
+    const patrimonioHistorico = response.planeamiento.patrimonioHistorico;
+    const perteneceArrabalFelipeII = response.planeamiento.parcela.perteneceArrabalFelipeII === 'true';
+    const perteneceAPE0001 = response.planeamiento.parcela.perteneceAPE0001 === 'true';
 
-    return bdcSearch(street).then(a => {
-        console.log(a);
-        let NDP = a.codigoNdps;
-        return planeamientoNdp(NDP).then(response => {
-            console.log(response);
-            const catalogo = response.catalogo;
-            const patrimonioHistorico = response.patrimonioHistorico;
-            const perteneceArrabalFelipeII = response.parcela.perteneceArrabalFelipeII === 'true';
-            const perteneceAPE0001 = response.parcela.perteneceAPE0001 === 'true';
-            console.log('[INFO] Catalogo');
-            console.log(catalogo);
-            console.log('[INFO] Patrimonio historico');
-            console.log(patrimonioHistorico);
-            console.log('[INFO] Pertenece felipe o ape:');
-            console.log(perteneceArrabalFelipeII, perteneceAPE0001);
-            let speechText = '';
-            const anyCatalogue = Object.keys(catalogo).find( key => catalogo[key] !== [] && catalogo[key][0] && catalogo[key][0].proteccionActual !== undefined);
-            const affectedPatrimonio = patrimonioHistorico && patrimonioHistorico.length !== 0;
+    let speechText = '';
+    const anyCatalogue = Object.keys(catalogo).find( key => catalogo[key] !== [] && catalogo[key][0] && catalogo[key][0].proteccionActual !== undefined);
+    const affectedPatrimonio = patrimonioHistorico && patrimonioHistorico.length !== 0;
 
-            speechText += anyCatalogue ? `La protección del edificio como ${anyCatalogue} es ${catalogo[anyCatalogue][0].proteccionActual} ` : 'El edificio no está protegido ';
-            speechText += affectedPatrimonio ? 'y está afectado por patrimonio de la Comunidad de Madrid. '
-                : 'y no está afectado por patrimonio de la Comunidad de Madrid. ';
+    speechText += anyCatalogue ? `La protección del edificio como ${anyCatalogue} es ${catalogo[anyCatalogue][0].proteccionActual} ` : 'El edificio no está protegido ';
+    speechText += affectedPatrimonio ? 'y está afectado por patrimonio de la Comunidad de Madrid. '
+        : 'y no está afectado por patrimonio de la Comunidad de Madrid. ';
 
-            if(perteneceArrabalFelipeII && perteneceAPE0001){
-                speechText += 'Además, pertenece al Arrabal de Felipe II y al APE0001.';
-            } else if(perteneceAPE0001) {
-                speechText += 'Además, pertenece al APE0001.';
-            } else if(perteneceArrabalFelipeII) {
-                speechText += 'Además, pertenece al Arrabal de Felipe II.';
-            }
-            return speechText;
-        }).catch( e => {
-            console.log(e.message)
-        })
-    })
+    if(perteneceArrabalFelipeII && perteneceAPE0001){
+        speechText += 'Además, pertenece al Arrabal de Felipe II y al APE0001.';
+    } else if(perteneceAPE0001) {
+        speechText += 'Además, pertenece al APE0001.';
+    } else if(perteneceArrabalFelipeII) {
+        speechText += 'Además, pertenece al Arrabal de Felipe II.';
+    }
+    return speechText;
 };
 
-const felipe = street => {
-    return bdcSearch(street).then(a => {
-        console.log(a);
-        let NDP = a.codigoNdps;
-        return planeamientoNdp(NDP).then(response => {
-            console.log(response);
-            const perteneceArrabalFelipeII = response.parcela.perteneceArrabalFelipeII === 'true';
-            console.log('[INFO] Pertenece felipe o ape:');
-            console.log(perteneceArrabalFelipeII);
-            const speechText = perteneceArrabalFelipeII ? `Sí, ${street} pertenece al Arrabal de Felipe II.` : `No, ${street} no pertenece al Arrabal de Felipe II.`;
-            console.log(speechText);
-            return speechText;
-        }).catch( e => {
-            console.log(e.message)
-        })
-    })
+const felipe = async street => {
+    let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
+    const response = address.planeamiento;    console.log(response);
+    const perteneceArrabalFelipeII = response.parcela.perteneceArrabalFelipeII === 'true';
+    console.log('[INFO] Pertenece felipe o ape:');
+    console.log(perteneceArrabalFelipeII);
+    const speechText = perteneceArrabalFelipeII ? `Sí, ${address.parsedStreet} pertenece al Arrabal de Felipe II.`
+        : `No, ${address.parsedStreet} no pertenece al Arrabal de Felipe II.`;
+    console.log(speechText);
+    return speechText;
 };
 
-const ape = street => {
-    return bdcSearch(street).then(a => {
-        console.log(a);
-        let NDP = a.codigoNdps;
-        return planeamientoNdp(NDP).then(response => {
-            console.log(response);
-            const perteneceAPE0001 = response.parcela.perteneceAPE0001 === 'true';
-            console.log('[INFO] Pertenece ape:');
-            console.log(perteneceAPE0001);
-            const speechText = perteneceAPE0001 ? `Sí, ${street} pertenece al APE0001.` : `No, ${street} no pertenece al APE0001.`;
-            console.log(speechText);
-            return speechText;
-        }).catch( e => {
-            console.log(e.message)
-        })
-    })
+const ape = async street => {
+    let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
+    const response = address.planeamiento;
+    console.log(response);
+    const perteneceAPE0001 = response.parcela.perteneceAPE0001 === 'true';
+    console.log('[INFO] Pertenece ape:');
+    console.log(perteneceAPE0001);
+    const speechText = perteneceAPE0001 ? `Sí, ${address.parsedStreet} pertenece al APE0001.`
+        : `No, ${address.parsedStreet} no pertenece al APE0001.`;
+    console.log(speechText);
+    return speechText;
 };
 
-const bip = street => {
-    return bdcSearch(street).then(a => {
-        console.log(a);
-        let NDP = a.codigoNdps;
-        return planeamientoNdp(NDP).then(response => {
-            console.log(response);
-            const patrimonioHistorico = response.patrimonioHistorico;
-            console.log('[INFO] patrimonioHistorico');
-            console.log(patrimonioHistorico);
-
-            const checkbip = (patrimonio) => patrimonio.categoria.includes("BIP");
-            
-            const speechText = patrimonioHistorico.some(checkbip) ? 
-            `${street} es un Bien de interés patrimonial` : `${street} no es un bien de interés patrimonial`
-            
-            return speechText;
-        }).catch( e => {
-            console.log(e.message)
-        })
-    })
+const bip = async street => {
+    let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
+    const response = address.planeamiento;
+    console.log(response);
+    const patrimonioHistorico = response.patrimonioHistorico;
+    console.log('[INFO] patrimonioHistorico');
+    console.log(patrimonioHistorico);
+    const checkBip = (patrimonio) => patrimonio.categoria.includes("BIP");
+    const speechText = patrimonioHistorico.some(checkBip) ?
+        `${address.parsedStreet} es un Bien de interés patrimonial` : `${address.parsedStreet} no es un bien de interés patrimonial`
+    return speechText;
 };
 
-const bic = street => {
-    return bdcSearch(street).then(a => {
-        console.log(a);
-        let NDP = a.codigoNdps;
-        return planeamientoNdp(NDP).then(response => {
-            console.log(response);
-            const patrimonioHistorico = response.patrimonioHistorico;
-            console.log('[INFO] patrimonioHistorico');
-            console.log(patrimonioHistorico);
-            
-            const checkbip = (patrimonio) => patrimonio.categoria.includes("BIC");
-            
-            const speechText = patrimonioHistorico.some(checkbip) ? 
-            `${street} es un Bien de interés cultural` : `${street} no es un bien de interés cultural`
-            
-            return speechText;
+const bic = async street => {
+    let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
+    const response = address.planeamiento;
 
-            return speechText;
-        }).catch( e => {
-            console.log(e.message)
-        })
-    })
+    console.log(response);
+    const patrimonioHistorico = response.patrimonioHistorico;
+    console.log('[INFO] patrimonioHistorico');
+    console.log(patrimonioHistorico);
+
+    const checkBic = (patrimonio) => patrimonio.categoria.includes("BIC");
+
+    const speechText = patrimonioHistorico.some(checkBic) ?
+        `${address.parsedStreet} es un Bien de interés cultural` : `${address.parsedStreet} no es un bien de interés cultural`
+
+    return speechText;
 };
 
-const catalogue = street => {
-    return bdcSearch(street).then(a => {
-        console.log(a);
-        let NDP = a.codigoNdps;
-        return planeamientoNdp(NDP).then(response => {
-            console.log(response);
-            const catalogo = response.catalogo;
-            console.log('[INFO] Catalogo');
-            console.log(catalogo);
-            let speechText = `La protección de ${street} `;
-            const protectedCat = Object.keys(catalogo).filter( key => {
-                try{
-                    console.log(catalogo[key][0].proteccionActual)
-                } catch (e) {
-                    console.log(e);
-                }
-                return catalogo[key] !== [] && catalogo[key][0] && catalogo[key][0].proteccionActual !== undefined;
-            });
-            console.log('[INFO] Protected cat:');
-            console.log(protectedCat);
-            if (protectedCat !== undefined && protectedCat.length > 0) {
-                protectedCat.map( (category, i) => speechText += `${i === protectedCat.length - 1 && protectedCat.length > 1 ? 'y ' : ', '} como ${category} es ${catalogo[category][0].proteccionActual}`);
-            } else {
-                speechText = 'El edificio no está protegido.'
-            }
-            console.log('[INFO] Speechtext')
-            console.log(speechText);
-            return speechText;
-        }).catch( e => {
-            console.log(e.message)
-        })
-    })
+const catalogue = async street => {
+    let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
+    const response = address.planeamiento;
+    console.log(response);
+    const catalogo = response.catalogo;
+    console.log('[INFO] Catalogo');
+    console.log(catalogo);
+    let speechText = `La protección de ${address.parsedStreet} `;
+    const protectedCat = Object.keys(catalogo).filter( key => {
+        try{
+            console.log(catalogo[key][0].proteccionActual)
+        } catch (e) {
+            console.log(e);
+        }
+        return catalogo[key] !== [] && catalogo[key][0] && catalogo[key][0].proteccionActual !== undefined;
+    });
+    console.log('[INFO] Protected cat:');
+    console.log(protectedCat);
+    if (protectedCat !== undefined && protectedCat.length > 0) {
+        protectedCat.map( (category, i) => speechText += `${i === protectedCat.length - 1 && protectedCat.length > 1 ? 'y ' : ', '} como ${category} es ${catalogo[category][0].proteccionActual}`);
+    } else {
+        speechText = 'El edificio no está protegido.'
+    }
+    console.log('[INFO] Speechtext')
+    console.log(speechText);
+    return speechText;
 };
 
 module.exports = {
