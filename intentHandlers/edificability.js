60f4488a28bab34c39a43c34753f0638b8f0f319
Alfonso Jiménez <aljimenez97@gmail.com>
2019-11-12 18:55:36 +0100

    🚧 Mail expediente

--- intentHandlers/edificability.js
+++ intentHandlers/edificability.js
@@ -4,6 +4,6 @@ module.exports = async (street) => {
     console.log('[INFO] edificability request handler: ?');
     let address = (typeof street === 'string') ? await getPlaneamiento(street) : street;
     let area = address.planeamiento.parcela.usos[0].usoEdificabilidad;
-    return (area==='---') ? `No dispongo de esa información sobre ${parsedStreet}` :
+    return (area==='---') ? `No dispongo de esa información sobre ${address.parsedStreet}` :
         `En ${address.parsedStreet} se puede construir ${area.replace(".", ",")} metros cuadrados del uso cualificado residencial vivienda colectiva. ¿Quieres preguntar por la normativa, protección o expediente en la misma ubicación?`;
 };
