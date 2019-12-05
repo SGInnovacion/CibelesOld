2a2453e1a1d29b1ec66593918c6e05c956e2cbe9
Daniel Carlander-Reuterfelt Gallo <d.cgallo@alumnos.upm.es>
2019-12-05 13:41:37 +0100

    ✨ getSuggestions en Dialogflow

--- dialogflow.js
+++ dialogflow.js
@@ -32,16 +32,19 @@ router.post('/', (request, response) => {
 
     const fallback = agent => recordQuery(agent, "Default fallback");
 
-    async function parseDialog(agent, intentHandler){
+    async function parseDialog(agent, intentHandler, newConsultName=[]){
         let street = agent.parameters.address;
+        let consulted = [];
 
         if (street.length > 0) {
             console.log('A new street was received');
+            consulted = newConsultName;
             // Aquí iría el código para guardar la nueva calle en la bbdd para los analytics.
         } else {
             try {
                 street = agent.getContext('session-variables').parameters.street;
                 console.log('We will be using the street stored in the session-variables');
+                consulted = [...new Set(agent.getContext('session-variables').parameters.consulted.concat(newConsultName))];
             } catch (e) {
                 console.log('There is no street stored, we will ask the user for one');
                 agent.add('¿Puedes decirme la calle?');
@@ -51,30 +54,56 @@ router.post('/', (request, response) => {
 
         agent.setContext({ name: 'session-variables',
             lifespan: 99999,
-            parameters: { street: street}
+            parameters: { 
+                street: street,
+                consulted: consulted
+            }
         });
+
+        console.log('Consulted: ', consulted)
 
         let out = await intentHandler(street);
         console.log(out);
-        agent.add(out);
+        agent.add(out + getSuggestions(consulted));
     }
 
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('Default Fallback', fallback);
-    intentMap.set('Edificability', agent => parseDialog(agent, getEdificability));
+    intentMap.set('Edificability', agent => parseDialog(agent, getEdificability, ['edificabilidad']));
     intentMap.set('General Info', agent => parseDialog(agent, getGeneralInfo));
     intentMap.set('Protection.catalogue', agent => parseDialog(agent, getProtection.catalogue));
-    intentMap.set('Protection.general', agent => parseDialog(agent, getProtection.general));
+    intentMap.set('Protection.general', agent => parseDialog(agent, getProtection.general, ['protección']));
     intentMap.set('Protection.felipe', agent => parseDialog(agent, getProtection.felipe));
     intentMap.set('Protection.ape', agent => parseDialog(agent, getProtection.ape));
     intentMap.set('Protection.bic', agent => parseDialog(agent, getProtection.bic));
     intentMap.set('Protection.bip', agent => parseDialog(agent, getProtection.bip));
-    intentMap.set('Regulations', agent => parseDialog(agent, getRegulations));
-    intentMap.set('Record', agent => parseDialog(agent, getRecord));
-    intentMap.set('Use', agent => parseDialog(agent, getUse));
+    intentMap.set('Regulations', agent => parseDialog(agent, getRegulations, ['normativa']));
+    intentMap.set('Record', agent => parseDialog(agent, getRecord, ['expediente']));
+    intentMap.set('Use', agent => parseDialog(agent, getUse, ['usos']));
     agent.handleRequest(intentMap);
 });
+
+const getSuggestions = (consulted=[]) => {
+    // let available = ['mail', 'edificabilidad', 'protección', 'expediente', 'normativa', 'usos']; // In case mails can be sent
+    let available = ['edificabilidad', 'protección', 'expediente', 'normativa', 'usos'];
+    if (consulted.length < available.length) {
+        let toConsult = available.filter( el => !consulted.includes(el) );
+        if (toConsult.includes("mail") && (!out.includes("No hay información") && !out.includes("no está protegido."))){
+            return '¿Quieres que te envíe un correo con la información que he encontrado?'
+        } else {
+            toConsult = toConsult.filter( el => el != 'mail').slice(0, 2);
+            const asserter = ["Puedes preguntar por ", "También te puedo informar sobre ", "Puedes consultar sobre ", "Puedo informarte sobre "].random();
+            const there = [' en la misma ubicación', ' en esa dirección', ' en el mismo lugar', ' en esa ubicación'].random();
+            return asserter + toConsult.join(' o ') + there;
+        }
+    }
+    return ''
+};
+
+Array.prototype.random = function(){
+    return this[Math.floor(Math.random()*this.length)];
+};
 
 app.use('/', router);
 module.exports = app;
