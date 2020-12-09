c023d01c2ae51581dcb779b065e7f291c7da7dc9
SGInnovacion <37309493+SGInnovacion@users.noreply.github.com>
2020-12-09 10:40:44 +0100

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,3 +1,17 @@
+/*
+* copyright 2020 Ayuntamiento de Madrid. Subdirección General de Innovación e Información Urbana
+* Licencia con arreglo a la EUPL, Versión 1.2 o –en cuanto sean aprobadas por la
+Comisión Europea– versiones posteriores de la EUPL (la «Licencia»);
+* Solo podrá usarse esta obra si se respeta la Licencia.
+* Puede obtenerse una copia de la Licencia en:
+* http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+* Salvo cuando lo exija la legislación aplicable o se acuerde por escrito, el programa
+distribuido con arreglo a la Licencia se distribuye «TAL CUAL», SIN GARANTÍAS NI
+CONDICIONES DE NINGÚN TIPO, ni expresas ni implícitas.
+* Véase la Licencia en el idioma concreto que rige los permisos y limitaciones que
+establece la Licencia.
+*/
+
 const parseGeneralInfo = parcela => {
     let out = '';
     try {
